1b3fe81a3f205d076d67dab894c5b2c7721ae7d3
WS Ang <weishengang123@gmail.com>
2023-02-20 20:28:59 +0800

    add some comment since i dk what i have wrote

--- yolo_seedpoint_look/src/yolo_seedpoint_look.py
+++ yolo_seedpoint_look/src/yolo_seedpoint_look.py
@@ -8,21 +8,32 @@ import os
 import time
 
 loop_count = 0
-
+feed_stats = 2
 #define a node then publish topic 'chatter' and also can subscribe topic
 rospy.init_node('YOLO')
+
+# def SeedFeeding():
+#     try:
+#         data0 = rospy.wait_for_message("/Arduino_SeedFeeding", String, timeout = 1)
+#         print(data0)
+#         length0 = len(data0.data)
+#         if length0 > 4:
+#             return 1
+#     except rospy.ROSException:
+#         pass
 
 def SeedRoll():
     #if run only once fail, then use for loop and run few more times
     pub = rospy.Publisher('Arduino_cmd_SR', String, queue_size=10) # call arduino roll the seed
     pub_command = "Roll"
+    print("Rolling Seed...")
     pub.publish(pub_command)
 
     try:
-        data = rospy.wait_for_message("/Arduino_SeedRolling", String, timeout = 1)
-        print(data)
-        length = len(data.data)
-        if length > 4:
+        data1 = rospy.wait_for_message("/Arduino_SeedRolling", String, timeout = 1)
+        length1 = len(data1.data)
+        if length1 > 4:
+            print("Seed Rolled\n")
             return 1
     except rospy.ROSException:
         pass
@@ -34,14 +45,14 @@ def SeedPlanting():
     #if run only once fail, then use for loop and run few more times
     pub2 = rospy.Publisher('Arduino_cmd_SP', String, queue_size=10) # seed position is right, call arduino plant it
     pub2_command = "Plant"
-    print("Planting")
+    print("Planting...")
     pub2.publish(pub2_command)
 
     try:
         data2 = rospy.wait_for_message("/Arduino_SeedPlanting", String, timeout = 1)
-        print(data2)
         length2 = len(data2.data)
         if length2 > 4:
+            print("Seed Planted\n")
             return 1
     except rospy.ROSException:
         pass
@@ -49,7 +60,7 @@ def SeedPlanting():
     rate = rospy.Rate(1000) #10hz
     rate.sleep()
 
-def yolo_detect():
+def yolo_detect(img_input):
     roll_cmd = 0
     # get the label names that named by us
     labelsPath = "yolov4/seed.names"
@@ -66,7 +77,7 @@ def yolo_detect():
     net = cv.dnn.readNetFromDarknet(configPath, weightsPath)
 
     #import image and define the height and weight
-    image = cv.imread("test4.jpg")
+    image = cv.imread(img_input)
     #imS = cv.resize(image, (960, 540))  #resize the window size of image show
     (H, W) = image.shape[:2]
 
@@ -112,37 +123,63 @@ def yolo_detect():
                 center_Y = int(boxes[i][1] + (boxes[i][3] / 2))
                 text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidences[i])
                 cv.putText(image, text, (x, y - 5), cv.FONT_HERSHEY_SIMPLEX,0.5, color, 2)
+
+                if classIDs[i] == 0:
+                    start_cmd = 1
+
                 if classIDs[i] == 1:
                     roll_cmd = 1
 
                 
     #cv.imshow("Image", image)
     #cv.waitKey(0)
-    return roll_cmd
+    return start_cmd, roll_cmd
 
 
-def main():
+
+
+def main(stat):
     # while True:
     #     feed_stats = SeedFeeding()
     #     if feed_stats == 1:
     #         break
     
-    cmd = yolo_detect()         # first we detect the seed first
-    # cmd = 0 # to test plant
+    while True:
+        
+        if stat % 2 != 0:
+            print("############ START ############\n")
+            yolo_input = "test4.jpg"
+        else:
+            yolo_input = "test.jpg"
+        stat =~ stat # =~ means not, which let 1 become 0, 0 become 1
+        
+        feed_stats, cmd = yolo_detect(yolo_input) # feed_stats = 1 means found seed, the seed drop successfully on fanzhuan
+                                                  # this  function is also use to proceed the cmd to decide whether roll or plant the seed
+        if feed_stats == 1: 
+            
+            # print("seed: ", feed_stats, " , cmd: ", cmd)
+            break
     
+    # cmd = yolo_detect()         # first we detect the seed first
+    # cmd = 0 # to test plant
+
     if cmd == 1:                # if detected seed point
+        print("芽點朝上")
         while True:             
                 roll_stats = SeedRoll()  # roll the seed
                 if roll_stats == 1:      # if seed roll done then break
                     break
 
     elif cmd == 0:
+        print("芽點朝下")
         while True:
             plant_stats = SeedPlanting() # plant the seed
             if plant_stats == 1:         # if plant done then break
+                print("############ END ############\n")
+                #exit()
                 break
     
 while True:
-    main()
-    loop_count = loop_count+ 1
-    print(loop_count)
+    loop_count = loop_count + 1
+    main(loop_count)
+    # print(loop_count)
